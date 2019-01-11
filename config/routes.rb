171bf77ccaa80abbe2bd0a50d55908fa0b2e6f53
fullstack99 <webdev0902@gmail.com>
2019-01-11 17:32:40 +0800

    create CURD and fil upload

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'resumes/index'
+  get 'resumes/new'
+  get 'resumes/create'
+  get 'resumes/destroy'
+  get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resources :comments
+  end
+
+  resources :resumes, only: [:index, :new, :create, :destroy]  
+
+  root 'welcome#index'
+end
